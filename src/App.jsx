1c1f27547c59f31ab9fbd1ae2035c51ba3143e0b
Eduard <eduard.ricardovs@gmail.com>
2025-01-10 22:43:02 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import confetti from "canvas-confetti";
+import "./App.css";
+import { Square } from "./components/Square";
+import { WinnerModal } from "./components/WinnerModal";
+import { TURNS } from "./constants";
+import { checkWinner } from "./logic/board";
+function App() {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage=localStorage.getItem('board');
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    const newWinner = checkWinner(newBoard);
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", turn);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (!newBoard.includes(null)) {
+      setWinner(false);
+    }
+  };
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn == TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn == TURNS.O}>{TURNS.O}</Square>
+      </section>
+      <WinnerModal resetGame={resetGame} winner={winner} />
+    </main>
+  );
+}
+
+export default App;
